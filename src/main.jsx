869559e756c89f0be20ebd2558a898149d0eaf96
jahangirahmed77 <jangeer9182@gmail.com>
2025-08-27 22:19:33 +0500

    c1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Add basename here */}
+    <BrowserRouter basename="/guardsosweb">
       <App />
     </BrowserRouter>
   </StrictMode>,
